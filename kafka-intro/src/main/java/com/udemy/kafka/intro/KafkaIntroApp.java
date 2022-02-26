bc869e42f3f05151ad7ed06ec893eb73b53e470b
DouglasGo8 <abraham.dias.douglas@gmail.com>
2022-02-26 12:58:15 -0300

    First Commit

---
+++ kafka-intro/src/main/java/com/udemy/kafka/intro/KafkaIntroApp.java
@@ -0,0 +1,16 @@
+package com.udemy.kafka.intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dougdb
+ */
+@SpringBootApplication
+public class KafkaIntroApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaIntroApp.class, args);
+  }
+
+}
