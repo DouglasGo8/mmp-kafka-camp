7ab471955c46340e5125e552656c512d795ff35b
DouglasGo8 <abraham.dias.douglas@gmail.com>
2022-03-05 18:26:20 -0300

    Twitter to Kafka done

--- kafka-twitter-integ/src/main/java/com/udemy/kafka/twitter/mediation/TwitterConsumerRoute.java
+++ kafka-twitter-integ/src/main/java/com/udemy/kafka/twitter/mediation/TwitterConsumerRoute.java
@@ -2,14 +2,53 @@ package com.udemy.kafka.twitter.mediation;
 
 import lombok.NoArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @author dougb
+ */
 @Component
 @NoArgsConstructor
 public class TwitterConsumerRoute extends RouteBuilder {
+
   @Override
-  public void configure() throws Exception {
-    from("twitter-search:java")
-            .log("Twitter Body - ${body}");
+  public void configure() {
+
+    onException(Exception.class)
+            .log("${body} fail");
+
+    from("twitter-search:" + this.rangeOfTerms())
+            //.marshal().json(JsonLibrary.Jackson)
+            .to("kafka:twitter_tweets")
+            //.log("${body}")
+            .end();
+            //.log("Twitter Body - ${body}");
+
+    /*from("timer://myTimerSearch?period=5s&fixedRate=true")
+            .transform(method(TwitterConsumerRoute.class, "rangeOfTerms"))
+            //.log("${body}")
+            .toD("twitter-search:${body}")
+            .marshal().json(JsonLibrary.Jackson)
+            .log("${body}")
+            .end();*/
+
+    /*from("timer://myTimer?period=5s&fixedRate=true")
+            .transform(method(TwitterConsumerRoute.class, "rangeOfTerms"))
+            .log("${body}")
+            .end();*/
+  }
+
+
+  /**
+   * When Camel in Consumer mode, just one word will be selected (singleton) in each boot
+   * @return
+   */
+  String rangeOfTerms() {
+    //
+    var terms = new String[]{"java", "python", "ukraine", "javascript", "apache camel", "kafka"};
+    return terms[ThreadLocalRandom.current().nextInt(0, terms.length - 1)];
   }
 }
